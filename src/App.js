f10cc4244c18f96e3d06e78d32537a4ea584caea
Helen <AlksAlena@users.noreply.github.com>
2017-09-11 19:04:54 +0600

    support local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import Desk from "./components/Desk";
+import NewUserPopup from "./components/NewUserPopup";
+
+// localStorage.clear();
 
 export default class App extends Component {
 	render() {
-		return (
-			<div className="app">
+		let author = localStorage.getItem("author");
+		if(author) {
+			console.log("Auhor: " + author);
+			return (
+				<div className="container app">
+					<div className="app-header">
+						<h1>Frello - powerful app for managing your tasks
+							<small>Get's started it's FREE!</small>
+						</h1>
+					</div>
+					<Desk />
+				</div>
+			);
+		} else return (
+			<div className="container app">
 				<div className="app-header">
 					<h1>Frello - powerful app for managing your tasks
 						<small>Get's started it's FREE!</small>
 					</h1>
 				</div>
 				<Desk />
+				<NewUserPopup />
 			</div>
 		);
 	}
