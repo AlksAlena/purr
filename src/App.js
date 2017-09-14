ac58506214bc7404110ddf0815279c084c8e676a
Helen <AlksAlena@users.noreply.github.com>
2017-09-14 23:49:08 +0600

    improvements

--- src/App.js
+++ src/App.js
@@ -2,32 +2,38 @@ import React, { Component } from "react";
 import Desk from "./components/Desk";
 import NewUserPopup from "./components/NewUserPopup";
 
-// localStorage.clear();
-
 export default class App extends Component {
-	render() {
-		let author = localStorage.getItem("author");
-		if(author) {
-			console.log("Auhor: " + author);
-			return (
-				<div className="container app">
-					<div className="app-header">
-						<h1>Frello - powerful app for managing your tasks
-							<small>Get's started it's FREE!</small>
-						</h1>
-					</div>
-					<Desk />
-				</div>
-			);
-		} else return (
+	constructor(props) {
+		super(props);
+		this.downloadAuthor = this.downloadAuthor.bind(this);
+		this.state = {
+			author: ""
+		}
+	}
+
+	downloadAuthor() {
+		let authorLS = localStorage.getItem("author");
+		(authorLS ? this.setState({ author: authorLS }): "");
+		
+	}
+
+	componentWillMount() {
+		this.downloadAuthor();
+	}
+
+	render() {		
+		return (
 			<div className="container app">
 				<div className="app-header">
-					<h1>Frello - powerful app for managing your tasks
+					{ this.state.author ? 
+						<h1>Hi, {this.state.author}</h1> : 
+						<h1>Frello - powerful app for managing your tasks
 						<small>Get's started it's FREE!</small>
-					</h1>
+						</h1>
+					}
 				</div>
 				<Desk />
-				<NewUserPopup />
+				{ this.state.author ? "" : <NewUserPopup app={this} /> }
 			</div>
 		);
 	}
