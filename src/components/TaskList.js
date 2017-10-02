fd68b504a69e0ef5a54dc12c914f061c29a5f456
Helen <AlksAlena@users.noreply.github.com>
2017-10-02 16:07:25 +0600

    не функциональные изменения
    1. изменен метод render(), используется стрелочная функция в map()$
    2. удалены методы del(), upd(), generateTask()$
    3. изменены передаваемые вниз параметры, использована деструктуризация props

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,40 +3,20 @@ import PropTypes from "prop-types";
 import Task from "./Task";
 
 export default class TaskList extends Component {
-	constructor(props) {
-		super(props);
-		this.generateTask = this.generateTask.bind(this);
-		this.delTask = this.delTask.bind(this);
-		this.updTask = this.updTask.bind(this);
-	}
-
-	delTask() {
-		this.props.column.delTask();
-	}
-
-	updTask() {
-		this.props.column.updTask();
-	}
-
-	generateTask(item, index) {
-		// task - current task
-		// del(), upd() - functions
-		// currentColumnTitle - current title of the column (may be change after init)
-		return (
+	render() {	
+		const { deleteTask, updateTask, initColumnTitle, customColumnTitle } = this.props;
+		let taskList = this.props.tasks.map((task, index) => 
 			<div key={index}>
-				<Task task={item} 
+				<Task 
+					task={task}
 					index={index} 
-					del={this.delTask} 
-					upd={this.updTask} 
-					initColumnTitle={this.props.column.props.title} 
-					currentColumnTitle={this.props.column.state.title}
+					deleteTask={deleteTask} 
+					updateTask={updateTask} 
+					initColumnTitle={initColumnTitle} 
+					customColumnTitle={customColumnTitle}
 				/>
 			</div>
 		);
-	}
-
-	render() {	
-		let taskList = this.props.tasks.map(this.generateTask);
 		return (
 			<div className="tasks">
 				{taskList}
@@ -47,5 +27,8 @@ export default class TaskList extends Component {
 
 TaskList.propTypes = {
 	tasks: PropTypes.array,
-	column: PropTypes.object,
+	deleteTask: PropTypes.func,
+	updateTask: PropTypes.func,
+	initColumnTitle: PropTypes.string,
+	customColumnTitle: PropTypes.string
 };
