0ba64834bd86fd9a2e0ecca0e746a8da62fbde73
Helen <AlksAlena@users.noreply.github.com>
2017-10-02 17:01:10 +0600

    не функциональные изменения
    1. добавлен метод handleChangeStateButton();
    2. изменены передаваемые вниз параметры

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,24 +6,23 @@ export default class Task extends Component {
 	constructor(props) {
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
-		this.update = this.update.bind(this);
-		this.delete = this.delete.bind(this);
+		this.updateTask = this.updateTask.bind(this);
+		this.deleteTask = this.deleteTask.bind(this);
+		this.handleChangeStateButton = this.handleChangeStateButton.bind(this);
 		this.state = {
 			isDetail: false,
 			isUpdate: false
 		};	
 	}
 
-	update() {
-		localStorage.setItem(`updIndex${this.props.initColumnTitle}`, this.props.index);
-		console.log(`create updIndex${this.props.initColumnTitle}`);
-		this.props.upd();
+	updateTask() {
+		localStorage.setItem(`updateIndex${this.props.initColumnTitle}`, this.props.index);
+		this.props.updateTask();
 	}
 
-	delete() {
-		localStorage.setItem(`delIndex${this.props.initColumnTitle}`, this.props.index);
-		console.log(`create delIndex${this.props.initColumnTitle}`);
-		this.props.del();
+	deleteTask() {
+		localStorage.setItem(`deleteIndex${this.props.initColumnTitle}`, this.props.index);
+		this.props.deleteTask();
 	}
 
 	handleClick() {
@@ -34,26 +33,40 @@ export default class Task extends Component {
 		}	
 	}
 
+	handleChangeStateButton() {
+		this.setState({ isDetail: false});
+	}
+
 	render() {
 		const divStyle = {
 			backgroundColor: "rgba(32, 178, 170, 0.5)"
 		};
+		const { title, comment } = this.props.task;
+
 		if(this.state.isDetail) {
 			return (
 				<div>
 					<div className="task" style={divStyle} onClick={this.handleClick} >
-						<p className="task-title">{this.props.task.title}
-							<i className="icon-comment-empty" title="comments">{this.props.task.comment.length}</i>
+						<p className="task-title">
+							{title}
+							<i className="icon-comment-empty" title="comments">{comment.length}</i>
 						</p>
 					</div>
-					<DetailTaskPopup task={this.props.task} parent={this} currentColumnTitle={this.props.currentColumnTitle}/>
+					<DetailTaskPopup 
+						task={this.props.task} 
+						currentColumnTitle={this.props.customColumnTitle} 
+						handleChangeStateButton={this.handleChangeStateButton} 
+						deleteTask={this.deleteTask} 
+						updateTask={this.updateTask}
+					/>
 				</div>
 			);
 		} else {
 			return (
 				<div className="task" onClick={this.handleClick} >
-					<p className="task-title">{this.props.task.title}
-						<i className="icon-comment-empty" title="comments">{this.props.task.comment.length}</i>
+					<p className="task-title">
+						{title}
+						<i className="icon-comment-empty" title="comments">{comment.length}</i>
 					</p>
 				</div>
 			);
@@ -65,8 +78,8 @@ export default class Task extends Component {
 Task.propTypes = {
 	task: PropTypes.object,
 	index: PropTypes.number,
-	del: PropTypes.func,
-	upd: PropTypes.func,
+	deleteTask: PropTypes.func,
+	updateTask: PropTypes.func,
 	initColumnTitle: PropTypes.string, 
-	currentColumnTitle: PropTypes.string,
+	customColumnTitle: PropTypes.string,
 };
