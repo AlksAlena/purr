70e4594df00f636b0e7fd2ee3d76cb4b006be146
Helen <AlksAlena@users.noreply.github.com>
2017-10-02 15:59:25 +0600

    не функциональные изменения
    1. изменены параметры, передаваемые во вложенные компоненты;
    2. добавлен метод handleChangeStateButton();
    3. изменен метод componentWillMount() на componentDidMount()

--- src/components/Column.js
+++ src/components/Column.js
@@ -15,66 +15,50 @@ export default class Column extends Component {
 		this.downloadTaskList = this.downloadTaskList.bind(this);
 		this.downloadColumnTitle = this.downloadColumnTitle.bind(this);
 		this.addTask = this.addTask.bind(this);
-		this.delTask = this.delTask.bind(this);
-		this.updTask = this.updTask.bind(this);
+		this.deleteTask = this.deleteTask.bind(this);
+		this.updateTask = this.updateTask.bind(this);
 		this.handleClick = this.handleClick.bind(this);
 		this.editColumnTitle = this.editColumnTitle.bind(this);
+		this.handleChangeStateButton = this.handleChangeStateButton.bind(this);
 	}
 
-	addTask() { // task download from localSrorage
-		let appStorage = localStorage;
-		// parse task in object
-		let task = JSON.parse(appStorage.getItem("task"));
+	addTask() {
+		let task = JSON.parse(localStorage.getItem("task"));
 		let oldTaskList = this.state.taskList;
-		// taskObject from localStorage
 		let newTaskList = oldTaskList.concat(task);
 		this.setState({ taskList: newTaskList });
-		// serialize object and upload in localStorage
 		var newTaskListSerial = JSON.stringify(newTaskList);
 		let nameColumn = this.props.title;
-		appStorage.setItem(`taskList${nameColumn}`, newTaskListSerial);
+		localStorage.setItem(`taskList${nameColumn}`, newTaskListSerial);
 		localStorage.removeItem("task");
 	}
 
-	delTask() {
-		console.log("start delete task");
+	deleteTask() {
 		let nameColumn = this.props.title;
-		// get index of elem for delete
 		let delIndex = localStorage.getItem(`delIndex${nameColumn}`);
-		console.log("key delete: " + delIndex);
-		// processing array (del elem)
 		let tmpTaskList = this.state.taskList;
 		tmpTaskList.splice(delIndex, 1);
-		// set new state - rerender and remove used delIndex
 		this.setState({ taskList: tmpTaskList });
 		localStorage.removeItem(`delIndex${nameColumn}`);
-		console.log("set new state and remove delIndex");
-		// serialize object and upload in localStorage taskList
 		var tmpTaskListSerial = JSON.stringify(tmpTaskList);
 		localStorage.setItem(`taskList${nameColumn}`, tmpTaskListSerial);
-		console.log("taskListLS update");
 	}
 
-	updTask() {
+	updateTask() {
 		let nameColumn = this.props.title;
-		console.log("update task ");
 		let updIndex = localStorage.getItem(`updIndex${nameColumn}`);
-		console.log("updIndex: " + updIndex);
-		// get Obj from LS - parse task in object
 		let updTask = JSON.parse(localStorage.getItem("updTask"));
-		// processing array (replacement elem)
 		let tmpTaskList = this.state.taskList;
 		tmpTaskList.splice(updIndex, 1, updTask);
-		// set new state - rerender and remove used updIndex
 		this.setState({ taskList: tmpTaskList });
 		localStorage.removeItem(`updIndex${nameColumn}`);
-		// remove used updTask object from LS
 		localStorage.removeItem("updTask");
-		console.log("set new state and remove updIndex");
-		// serialize object and upload in localStorage taskList
 		var tmpTaskListSerial = JSON.stringify(tmpTaskList);
 		localStorage.setItem(`taskList${nameColumn}`, tmpTaskListSerial);
-		console.log("taskListLS update");
+	}
+
+	handleChangeStateButton() {
+		this.setState({ isChangeTitle: false });
 	}
 
 	handleClick() {
@@ -86,7 +70,6 @@ export default class Column extends Component {
 	}
 
 	editColumnTitle() {
-		// download new title from LS
 		let newTitle = localStorage.getItem(`columnTitle${this.props.title}`);
 		this.setState({ title: newTitle });
 		this.setState({ isChangeTitle: false });
@@ -94,25 +77,21 @@ export default class Column extends Component {
 
 	downloadColumnTitle() {	
 		let nameColumn = this.props.title;
-		// if exist custom title for the column - download	
-		(
-			localStorage.getItem(`columnTitle${nameColumn}`) ? 
-				this.setState({ title: localStorage.getItem(`columnTitle${nameColumn}`) }) :
-				this.setState({ title: nameColumn })
+		( localStorage.getItem(`columnTitle${nameColumn}`) ? 
+			this.setState({ title: localStorage.getItem(`columnTitle${nameColumn}`) }) :
+			this.setState({ title: nameColumn })
 		);
 	}
 
 	downloadTaskList() {
 		let nameColumn = this.props.title;
-		// if exist taskList for the column - download
 		if(localStorage.getItem(`taskList${nameColumn}`)) {
 			let taskListLS = JSON.parse(localStorage.getItem(`taskList${nameColumn}`));
 			this.setState({ taskList: taskListLS });
 		}
 	}
 
-	componentWillMount() {
-		// download exist title and taskList from LS
+	componentDidMount() {
 		this.downloadColumnTitle();
 		this.downloadTaskList();
 	}
@@ -123,10 +102,21 @@ export default class Column extends Component {
 				<h2 className="column-title">{this.state.title}
 					<i className="icon-pencil" title="edit title" onClick={this.handleClick}></i>
 				</h2>
-				<TaskList tasks={this.state.taskList} column={this} />
-				<Button column={this} />
+				<TaskList 
+					tasks={this.state.taskList} 
+					deleteTask={this.deleteTask} 
+					updateTask={this.updateTask} 
+					initColumnTitle={this.props.title} 
+					customColumnTitle={this.state.title}
+				/>
+				<Button addTask={this.addTask} columnTitle={this.state.title} />
 				{ this.state.isChangeTitle ?
-					<ColumnNewTitlePopup parent={this} /> : ""
+					<ColumnNewTitlePopup 
+						handleChangeStateButton={this.handleChangeStateButton} 
+						editColumnTitle={this.editColumnTitle} 
+						initColumnTitle={this.props.title} 
+						customColumnTitle={this.state.title}
+					/> : ""
 				}
 			</div>
 		);
