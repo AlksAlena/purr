597a1a9e329b79622aa855c5b67545db7d06fa20
Helen <AlksAlena@users.noreply.github.com>
2017-11-07 11:48:19 +0600

    Tab -> space

--- src/reducers/makeRootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { popupsReducer } from './popups';
-import { columnsReducer } from './columns';
-
-export default combineReducers({
-	columnsReducer,
-	popupsReducer
-});
