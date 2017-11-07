ac6e9cda9c82de717477904999d1207e9d24927a
Helen <AlksAlena@users.noreply.github.com>
2017-11-07 11:45:35 +0600

    Форматирование

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import makeRootReducer from './reducers/makeRootReducer';
+
+import App from './App.jsx';
+
+const store = createStore(makeRootReducer, composeWithDevTools(applyMiddleware(thunk)));
+window.store = store;
+
+store.subscribe(() => {
+  let tmpDeskState = store.getState();
+  let tmpDeskStateSerial = JSON.stringify(tmpDeskState);
+  localStorage.setItem('deskState', tmpDeskStateSerial);
+}); 
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
